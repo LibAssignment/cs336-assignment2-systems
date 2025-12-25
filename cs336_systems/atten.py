cfb40b8e174b8e2f163bb31641e663ffd8f66430
Clouds Flowing <clouds.flowing@gmail.com>
2025-12-25 22:30:33 +0800

    add D_i to improve numeric stable

--- cs336_systems/atten.py
+++ cs336_systems/atten.py
@@ -18,7 +18,7 @@ class FlashAttnVanilla(Function):
   @staticmethod
   def forward(ctx: FunctionCtx, Q: Tensor, K: Tensor, V: Tensor, is_causal=False):
     # TODO: warning?
-    O, L = _vanilla_flash_attention_forward(Q, K, V, is_causal)
+    O, L, _ = _vanilla_flash_attention_forward(Q, K, V, is_causal=is_causal)
     L = L.squeeze(-1)  # remove last dim
     ctx.save_for_backward(L, Q, K, V, O)
     return O
@@ -45,26 +45,26 @@ class FlashAttnTorch(Function):
     assert K.shape[-2] % BLOCK_K == 0, "FlashAttnTorch only supports sequence length divisible by BLOCK_SIZE"
     assert K.shape[-2] == V.shape[-2], "FlashAttnTorch requires K and V to have the same sequence length"
 
-    Os = []
-    Ls = []
+    O = torch.zeros((*Q.shape[:-1], V.shape[-1]), device=Q.device, dtype=Q.dtype)
+    L = torch.full((*Q.shape[:-1], 1), float('-inf'), device=Q.device, dtype=Q.dtype)
     for i in range(Q.shape[-2] // BLOCK_Q):
       Q_i = Q[..., i*BLOCK_Q:(i+1)*BLOCK_Q, :]
       O_i = torch.zeros((*Q_i.shape[:-1], V.shape[-1]), device=Q.device, dtype=Q.dtype)
-      L_i = torch.zeros((*Q_i.shape[:-1], 1), device=Q.device, dtype=Q.dtype)
+      L_i = torch.full((*Q_i.shape[:-1], 1), float('-inf'), device=Q.device, dtype=Q.dtype)
+      D_i = None
 
       for j in range(K.shape[-2] // BLOCK_K):
         K_j = K[..., j*BLOCK_K:(j+1)*BLOCK_K, :]
         V_j = V[..., j*BLOCK_K:(j+1)*BLOCK_K, :]
 
-        O_ij, L_ij = _vanilla_flash_attention_forward(Q_i, K_j, V_j, is_causal)
-        L_new = (1 + (L_ij - L_i).exp()).log() + L_i
+        O_ij, L_ij, D_i = _vanilla_flash_attention_forward(Q_i, K_j, V_j, D=D_i, is_causal=is_causal)
+        L_new = ((L_i - D_i).exp() + (L_ij - D_i).exp()).log() + D_i
         O_i = O_i * (L_i - L_new).exp() + O_ij * (L_ij - L_new).exp()
         L_i = L_new
       assert O_i is not None and L_i is not None, "No blocks were processed in FlashAttnTorch forward pass."
-      Os.append(O_i)
-      Ls.append(L_i)
-    O = torch.cat(Os, dim=-2)
-    L = torch.cat(Ls, dim=-2).squeeze(-1)  # remove last dim
+      O[..., i*BLOCK_Q:(i+1)*BLOCK_Q, :] = O_i
+      L[..., i*BLOCK_Q:(i+1)*BLOCK_Q, :] = L_i
+    L = L.squeeze(-1)  # remove last dim
     assert O.shape == (*Q.shape[:-1], V.shape[-1]), "Output shape mismatch in FlashAttnTorch forward pass. from {} {}, got {}".format(Q.shape, V.shape, O.shape)
     assert L.shape == Q.shape[:-1], "Log-sum-exp shape mismatch in FlashAttnTorch forward pass. from {} got {}".format(Q.shape, L.shape)
     ctx.save_for_backward(L, Q, K, V, O)
@@ -75,7 +75,7 @@ class FlashAttnTorch(Function):
     return FlashAttnVanilla.backward(ctx, *grad_outputs)
 
 
-def _vanilla_flash_attention_forward(Q: Tensor, K: Tensor, V: Tensor, is_causal=False):
+def _vanilla_flash_attention_forward(Q: Tensor, K: Tensor, V: Tensor, *, D: Tensor | None = None, is_causal=False):
   d_k = torch.tensor(K.shape[-1])
   with nvtx.range("attention_scores"):
     atten = einsum(Q, K, "... queries d_k, ... keys d_k -> ... queries keys") / d_k.sqrt()
@@ -83,11 +83,12 @@ def _vanilla_flash_attention_forward(Q: Tensor, K: Tensor, V: Tensor, is_causal=
   #   with nvtx.range("attention_masking"):
   #     atten = atten.masked_fill(~mask, -torch.inf)
   with nvtx.range("attention_softmax"):
-    D = torch.max(atten, dim=-1, keepdim=True).values
+    _D = torch.max(atten, dim=-1, keepdim=True).values
+    D = torch.maximum(D, _D) if D is not None else _D
     atten = (atten - D).exp()
     L = atten.sum(dim=-1, keepdim=True)
     atten = atten / L
     L = L.log() + D
   with nvtx.range("attention_weighted_sum"):
     result = einsum(atten, V, "... queries keys, ... keys d_v -> ... queries d_v")
-  return result, L
+  return result, L, D
